9ee0adb9c1af81843fceddbd339857700b48d4cf
runyud <runyud@umich.edu>
2025-08-03 22:53:58 -0400

    calculate avg ratings

--- src/ratings/models.py
+++ src/ratings/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Avg
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
@@ -14,6 +15,18 @@ class RatingChoice(models.IntegerChoices):
     FIVE = 5
     __empty__ = 'Rate this'
 
+class RatingQuerySet(models.QuerySet):
+    def avg(self):
+        return self.aggregate(average=Avg('value'))['average'] # {"average": 1.2}
+
+
+class RatingManager(models.Manager):
+    def get_queryset(self):
+        return RatingQuerySet(self.model, using=self._db)
+    
+    def avg(self):
+        return self.get_queryset().avg()
+
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -23,3 +36,5 @@ class Rating(models.Model):
     content_object = GenericForeignKey("content_type", "object_id")
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = RatingManager() 
